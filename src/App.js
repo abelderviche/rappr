ec0de2e59ef5e8ac7db826cd3d5b0ea6784897ba
Abel Derviche - TTS Viajes <aderviche@ttsviajes.com>
2019-01-04 17:56:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 import Header from './Header/';
 import Search from './Search/';
+import './assets/sass/index.scss'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="main">
         <Header/>
         <Search />
       </div>
